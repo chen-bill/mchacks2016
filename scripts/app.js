a6a64877392b6f00759db4044768415ef13bcf2a
Bill Chen <chen.bill96@gmail.com>
2016-02-20 18:08:38 -0500

    outputs lists of stuff

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,10 @@ angular.module('mainApp', [])
 	$scope.page = "landingPage";
 	$scope.location = "";
 	$scope.selectedOptions = {};
-	$scope.loadedOption = {
-		hotelOptions: [],
-		attractionsOptions: [],
-		selectedEvents: []
+	$scope.loadedOptions = {
+		hotelOptions: {},
+		attractionsOptions: {},
+		restaurantOptions: {}
 	};
 	$scope.mapMap = {
 		toronto: {
@@ -22,10 +22,11 @@ angular.module('mainApp', [])
 		$scope.page = 'selectPage';
 		$http.get('http://api.tripadvisor.com/api/partner/2.0/search/' + queryLocation + '?key=' + tripAdvisorApiKey + '&categories=geos')
 			.then(function(res){
-				console.log(res.data.geos[0].location_id);
+				var newLocationId = res.data.geos[0].location_id;
 				$scope.selectedOptions.locationId = res.data.geos[0].location_id;
-				queryAttractionsByLocationId($scope.selectedOptions.locationId)
-				console.log($scope.selectedOptions);
+				queryAttractionsByLocationId(newLocationId);
+				queryHotelsByLocationId(newLocationId);
+				queryRestaurantsByLocationId(newLocationId);
 			}, function(err){
 				console.log(err);
 			});
@@ -34,16 +35,75 @@ angular.module('mainApp', [])
 	function queryAttractionsByLocationId(locationId) {
 		$http.get('http://api.tripadvisor.com/api/partner/2.0/location/' + locationId + '/attractions?key=' + tripAdvisorApiKey).then(querySuccess, queryError);
 
-		function querySuccess(res){
-			console.log(res);
-		}
-
+		function querySuccess(httpResponse){
+			console.log("query sucess");
+			parseData('attractions', httpResponse);
+		};
 		function queryError(err){
 			console.log(err);
+		};
+	}
+
+	function queryHotelsByLocationId(locationId){
+		$http.get('http://api.tripadvisor.com/api/partner/2.0/location/' + locationId + '/hotels?key=' + tripAdvisorApiKey).then(querySuccess, queryError);
+
+		function querySuccess(httpResponse){
+			parseData('hotels', httpResponse);
+		};
+		function queryError(err){
+			console.log(err);
+		};
+	}
+
+	function queryRestaurantsByLocationId(locationId){
+		$http.get('http://api.tripadvisor.com/api/partner/2.0/location/' + locationId + '/restaurants?key=' + tripAdvisorApiKey).then(querySuccess, queryError);
+
+		function querySuccess(httpResponse){
+			parseData('restaurants', httpResponse);
+		};
+		function queryError(err){
+			console.log(err);
+		};
+	}
+
+	function parseData(type, httpResponse){
+		if(type == 'attractions'){
+			attractionsArray = httpResponse.data.data;
+			for(var key in attractionsArray){
+				$scope.loadedOptions.attractionsOptions[attractionsArray[key].name] = {
+					name: attractionsArray[key].name,
+					latitude: attractionsArray[key].latitude,
+					longitude: attractionsArray[key].longitude,
+					rating: attractionsArray[key].rating,
+					address: attractionsArray[key].address_obj
+				}
+			}
+		} else if (type == 'restaurants'){
+			restaurantsArray = httpResponse.data.data;
+			for(var key in restaurantsArray){
+				$scope.loadedOptions.restaurantOptions[restaurantsArray[key].name] = {
+					name: restaurantsArray[key].name,
+					latitude: restaurantsArray[key].latitude,
+					longitude: restaurantsArray[key].longitude,
+					rating: restaurantsArray[key].rating,
+					address: restaurantsArray[key].address_obj
+				}
+			}
+		} else {
+			hotelOptions = httpResponse.data.data;
+			for(var key in hotelOptions){
+				$scope.loadedOptions.hotelOptions[hotelOptions[key].name] = {
+					name: hotelOptions[key].name,
+					latitude: hotelOptions[key].latitude,
+					longitude: hotelOptions[key].longitude,
+					rating: hotelOptions[key].rating,
+					address: hotelOptions[key].address_obj
+				}
+			}
 		}
 	}
 
-	function parseData(){
-
+	$scope.debug = function(){
+		console.log($scope.loadedOptions);
 	}
 }])
