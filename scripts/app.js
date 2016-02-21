cf451eb7e6f8e9dae06cfd8123aeecea6ac4f860
Milan Dimic <mdimic3112@gmail.com>
2016-02-21 03:47:40 -0500

    testing map

--- scripts/app.js
+++ scripts/app.js
@@ -182,6 +182,7 @@ angular.module('mainApp', ['ui.bootstrap', 'ngAnimate'])
 
 	$scope.generateItinerary = function(queryLocation){
 		$scope.page = 'resultPage';
+		initialize();
 		generateRouteData($scope.selectedEvents, function(response){
 			var req = {
 				method: 'POST',
@@ -251,4 +252,106 @@ angular.module('mainApp', ['ui.bootstrap', 'ngAnimate'])
 	 	$scope.location = $label;
 	    $scope.queryLocation($label);
 	};
+
+
+
+
+
+
+	var map;
+	var infoWindow;
+
+
+	var markersData = [
+	   {
+	  lat: 40.6386333,
+	      lng: -8.745,
+	      name: "1",
+
+	   },
+	   {
+	       lat: 40.59955,
+	      lng: -8.7498167,
+	      name: "2",
+
+	   },
+	   {
+	     lat: 40.6247167,
+	      lng: -8.7129167,
+	      name: "3",
+	 
+	   } 
+	];
+
+
+	function initialize() {
+		console.log("Init called");
+	   var mapOptions = {
+	      center: new google.maps.LatLng(40.601203,-8.668173),
+	      zoom: 7,
+	      mapTypeId: 'roadmap',
+	   };
+
+	   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	   // a new Info Window is created
+	   infoWindow = new google.maps.InfoWindow();
+
+	   // Event that closes the Info Window with a click on the map
+	   google.maps.event.addListener(map, 'click', function() {
+	      infoWindow.close();
+	   });
+
+	   // Finally displayMarkers() function is called to begin the markers creation
+	   displayMarkers();
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+
+	// This function will iterate over markersData array
+	// creating markers with createMarker function
+	function displayMarkers(){
+
+	   // this variable sets the map bounds according to markers position
+	   var bounds = new google.maps.LatLngBounds();
+	   
+	   // for loop traverses markersData array calling createMarker function for each marker 
+	   for (var i = 0; i < markersData.length; i++){
+
+	      var latlng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
+	      var name = markersData[i].name;
+	    
+	      createMarker(latlng, name);
+
+	      // marker position is added to bounds variable
+	      bounds.extend(latlng);  
+	   }
+
+
+	   map.fitBounds(bounds);
+	}
+
+	// This function creates each marker and it sets their Info Window content
+	function createMarker(latlng, name){
+	   var marker = new google.maps.Marker({
+	      map: map,
+	      position: latlng,
+	      title: name
+	   });
+
+	   google.maps.event.addListener(marker, 'click', function() {
+	      
+	      // Creating the content to be inserted in the infowindow
+	      var iwContent = '<div id="iw_container">' +
+	            '<div class="iw_title">' + name + '</div>' +
+	         '<div class="iw_content">' + address1 + '<br />' +
+	         address2 + '<br />' +
+	         postalCode + '</div></div>';
+	      
+	      infoWindow.setContent(iwContent);
+
+	      infoWindow.open(map, marker);
+	   });
+	}
+
 }])
